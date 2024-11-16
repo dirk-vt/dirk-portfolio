c29442b3a6468db71b4e4e2f63ade9ba7b7cd0dc
Dirk van Tonder <dirkvt14@gmail.com>
2024-11-16 16:35:11 +0200

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,121 @@
+import React from 'react'
+
+import { ButtonPrimary } from './Button';
+
+const sitemap = [
+    {
+      label: 'Home',
+      href: '#home'
+    },
+    {
+      label: 'About Me',
+      href: '#about'
+    },
+    {
+      label: 'Portfolio',
+      href: '#portfolio'
+    },
+    {
+      label: 'Contact me',
+      href: '#contact'
+    }
+  ];
+  
+  const socials = [
+    {
+      label: 'GitHub',
+      href: 'https://github.com/dirk-vt'
+    },
+    {
+      label: 'LinkedIn',
+      href: 'https://www.linkedin.com/in/dirk-van-tonder'
+    },
+
+  ];
+
+const Footer = () => {
+  return (
+    <footer className="section">
+        <div className="container">
+
+            <div className="lg:grid lg:grid-cols-2">
+
+                <div className="mb-10">
+                    <h2 className="headline-1 mb-8 lg:max-w-[12ch]">
+                        Let&apos;s work together today! 
+                    </h2>
+
+                    <ButtonPrimary
+                        href="mailto:dirkvt14@gmail.com"
+                        label="Start porject"
+                        icon="chevron_right"
+                    />
+                </div>
+
+                <div className="grid grid-cols-2 gap-4 lg:pl-20"> 
+
+                    <div>
+                        <p className="mb-2">Sitemap</p>
+
+                        <ul>
+                            {sitemap.map(({ label, href }, key) => (
+                                <li key={key}>
+                                    <a
+                                        href={href}
+                                        className="block text-sm
+                                        text-zinc-400 py-1 transition-colors hover:text-zinc-200"
+                                    >
+                                        {label}
+                                    </a>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+
+                    <div>
+                        <p className="mb-2">Socials</p>
+
+                        <ul>
+                            {socials.map(({ label, href }, key) => (
+                                <li key={key}>
+                                    <a
+                                        href={href}
+                                        target="_blank"
+                                        className="block text-sm
+                                        text-zinc-400 py-1 transition-colors hover:text-zinc-200"
+                                    >
+                                        {label}
+                                    </a>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+
+                </div>
+
+            </div>  
+
+            <div className="flex items-center justify-between pt-10 mb-8">
+                <a
+                  href=""
+                  className=""
+                >
+                    <img
+                      src="/images/logo.svg"
+                      width={40}
+                      height={40}
+                      alt="Logo" 
+                    />
+                </a>
+
+                <p className="text-zinc-500 text-sm">
+                    &copy; 2024 <span className="text-zinc-200">Dirk van Tonder</span>
+                </p>
+            </div>
+
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/components/Hero.jsx
@@ -0,0 +1,75 @@
+import React from 'react'
+
+import { ButtonPrimary, ButtonOutline } from './Button'
+
+const Hero = () => {
+  return (
+   <section
+    id="home"
+    className="pt-28 lg:pt-36"
+   >
+
+    <div className="container items-center lg:grid lg:grid-cols-2 lg:gap-10">
+
+        <div>
+
+            <div className="flex items-center gap-3">
+                <figure className="img-box w-9 h-9 rounded-full">
+                    <img
+                     src="/images/avatar-1.jpg"
+                     width={40}
+                     height={40}
+                     alt="Dirk van Tonder portrait"
+                     className="img-cover"
+                     />
+                </figure>
+
+                <div className="flex items-center gap-1.5 text-zinc-400 text-sm tracking-wide">
+                    <span className="relative w-1 h-3 rounded-full bg-emerald-400">
+                        <span className="absolute inset-0 rounded-full bg-emerald-400 animate-ping"></span>
+                    </span>
+
+                    Available for work
+                </div>
+            </div>
+
+            <h2 className="headline-1 max-w-[15ch] sm:max-w-[20ch] lg:max-w-[15ch] mt-5 mb-8 lg:mb-10">
+            Building Scalable Modern Websites for the Future    
+            </h2>
+
+            <div className="flex items-center gap-3">
+                <ButtonPrimary
+                    href="https://docs.google.com/document/d/e/2PACX-1vQGULmSDM0umoUjtfdhzeFtZHa046MeyErsoj2fWTHSeyMDqNcGbLp8Vr3Jv8Zo3z4DMvSYzuSM18ke/pub"
+                    label="View Resume"
+                    icon="download"
+                />
+
+                <ButtonOutline
+                href="#about"
+                label="Scroll donw"
+                icon="arrow_downward"
+                />
+            </div>
+
+        </div>
+
+        <div className="hidden lg:block">
+            <figure className="w-full max-w-[480px] ml-auto bg-gradient-to-t
+             from-sky-400 via-25% via-sky-40/40 to-55% rounded-[900px] overflow-hidden">
+                <img
+                 src="/images/hero-banner.png"
+                 width={656}
+                 height={800}
+                 alt="Dirk van Tonder"
+                 className="w-full"
+                />
+            </figure>
+        </div>
+
+    </div>
+
+   </section> 
+  )
+}
+
+export default Hero
