a0fa012f8f30c0ab76703aa807cb5dc4278757d3
Dirk van Tonder <140878138+dirk-vt@users.noreply.github.com>
2024-11-17 14:33:00 +0200

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -29,11 +29,11 @@ const Contact = () => {
 
             <div className="mb-12 lg:mb-0 lg:flex lg:flex-col">
 
-                <h2 className="headline-2 lg:max-w-[12ch]">
+                <h2 className="headline-2 lg:max-w-[12ch] reveal-up">
                 Contact me for work 
                 </h2>
 
-                <p className="text-zinc-400 mt=3 mb-8 max-w-[50ch] lg:max-w-[30ch]">
+                <p className="text-zinc-400 mt=3 mb-8 max-w-[50ch] lg:max-w-[30ch] reveal-up">
                 Reach out today to discuss your project needs and start collaborating on something amazing!
                 </p>
 
@@ -45,7 +45,7 @@ const Contact = () => {
                         target="_blank"
                         className="w-12 h-12 grid place-items-center ring-inset ring-2
                         ring-zinc-50/5 rounded-lg transition-[background-color,color] 
-                        hover:bg-zinc-50 hover:text-zinc-950 active:bg-zinc-50/80" 
+                        hover:bg-zinc-50 hover:text-zinc-950 active:bg-zinc-50/80 reveal-up" 
                         >
                             {icon}
                         </a>
@@ -63,7 +63,7 @@ const Contact = () => {
                     <div className="mb-4">
                         <label
                             htmlFor="name"
-                            className="label"
+                            className="label reveal-up"
                         >
                             Name
                         </label>
@@ -75,13 +75,13 @@ const Contact = () => {
                             autoComplete="name"
                             required
                             placeholder="Name"
-                            className="text-field"
+                            className="text-field reveal-up"
                         />
                     </div>
                     <div className="mb-4">
                         <label
                             htmlFor="email"
-                            className=""
+                            className=" label reveal-up"
                         >
                             Email
                         </label>
@@ -93,7 +93,7 @@ const Contact = () => {
                             autoComplete="email"
                             required
                             placeholder="email@example.com"
-                            className="text-field"
+                            className="text-field reveal-up"
                         />
                     </div>
                 </div> 
@@ -101,7 +101,7 @@ const Contact = () => {
                 <div className="mb-4">
                     <label
                         htmlFor="message"
-                        className="label"
+                        className="label reveal-up"
                     >   
                         Message
                     </label>
@@ -111,7 +111,7 @@ const Contact = () => {
                         id="message"
                         placeholder="Hi!"
                         required
-                        className="text-field resize-y min-h-32 max-h-80"
+                        className="text-field resize-y min-h-32 max-h-80 reveal-up"
                     >
 
                     </textarea>
@@ -119,7 +119,7 @@ const Contact = () => {
 
                 <button 
                     type="submit"
-                    className="btn btn-primary [&]:max-w-full w-full justify-center"
+                    className="btn btn-primary [&]:max-w-full w-full justify-center reveal-up"
                 >   
                     Submit
                 </button>
