64db17d6179d4b41acfbcf3ff8418d5f976571c7
Dirk van Tonder <140878138+dirk-vt@users.noreply.github.com>
2024-11-17 14:32:24 +0200

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -41,7 +41,7 @@ const Footer = () => {
             <div className="lg:grid lg:grid-cols-2">
 
                 <div className="mb-10">
-                    <h2 className="headline-1 mb-8 lg:max-w-[12ch]">
+                    <h2 className="headline-1 mb-8 lg:max-w-[12ch] reveal-up">
                         Let&apos;s work together today! 
                     </h2>
 
@@ -49,13 +49,14 @@ const Footer = () => {
                         href="mailto:dirkvt14@gmail.com"
                         label="Start porject"
                         icon="chevron_right"
+                        classes="reveal-up"
                     />
                 </div>
 
                 <div className="grid grid-cols-2 gap-4 lg:pl-20"> 
 
                     <div>
-                        <p className="mb-2">Sitemap</p>
+                        <p className="mb-2 reveal-up">Sitemap</p>
 
                         <ul>
                             {sitemap.map(({ label, href }, key) => (
@@ -63,7 +64,7 @@ const Footer = () => {
                                     <a
                                         href={href}
                                         className="block text-sm
-                                        text-zinc-400 py-1 transition-colors hover:text-zinc-200"
+                                        text-zinc-400 py-1 transition-colors hover:text-zinc-200 reveal-up"
                                     >
                                         {label}
                                     </a>
@@ -73,7 +74,7 @@ const Footer = () => {
                     </div>
 
                     <div>
-                        <p className="mb-2">Socials</p>
+                        <p className="mb-2 reveal-up">Socials</p>
 
                         <ul>
                             {socials.map(({ label, href }, key) => (
@@ -82,7 +83,7 @@ const Footer = () => {
                                         href={href}
                                         target="_blank"
                                         className="block text-sm
-                                        text-zinc-400 py-1 transition-colors hover:text-zinc-200"
+                                        text-zinc-400 py-1 transition-colors hover:text-zinc-200 reveal-up"
                                     >
                                         {label}
                                     </a>
@@ -97,8 +98,8 @@ const Footer = () => {
 
             <div className="flex items-center justify-between pt-10 mb-8">
                 <a
-                  href=""
-                  className=""
+                  href="/"
+                  className="logo reveal-up"
                 >
                     <img
                       src="/images/logo.svg"
@@ -108,7 +109,7 @@ const Footer = () => {
                     />
                 </a>
 
-                <p className="text-zinc-500 text-sm">
+                <p className="text-zinc-500 text-sm reveal-up">
                     &copy; 2024 <span className="text-zinc-200">Dirk van Tonder</span>
                 </p>
             </div>
