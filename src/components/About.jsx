beda8ec7916ede7c924f7c0727b96ab00bba736d
Dirk van Tonder <140878138+dirk-vt@users.noreply.github.com>
2024-11-19 14:02:45 +0200

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,9 +11,9 @@ const About = () => {
 
             <div className="bg-zinc-300/20 p-7 rounded-2xl md:p-12 reveal-up ">
                 <p className="text-zinc-950 mb-4 md:mb-8 md:text-xl md:max-w-[60ch]">
-                 Welcome! I'm Dirk van Tonder, a UX/UI designer and front-end developer with a passion for crafting intuitive,
-                 responsive digital experiences. With certifications in Google UX Design and Meta Front-End Development,
-                 I bring creativity, attention to detail, and a commitment to delivering high-quality work.
+                 Hello! I’m Dirk van Tonder, a UX/UI designer and front-end developer driven by a passion for crafting seamless, user-friendly digital experiences. 
+                With certifications in Google UX Design and Meta Front-End Development, I bring a blend of creativity, technical expertise, 
+                  and meticulous attention to detail to every project. My goal is to deliver innovative, high-quality solutions that exceed expectations.
                 </p>  
             </div>
 
